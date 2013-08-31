924b258778da42761eb1ba61aa5c5ec2f630c80e
sighmon <simon.loffler@gmail.com>
2013-08-31 15:01:51 +0930

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hackerspace Adelaide
+## Membership website
+
+The beginnings of a membership website for the Hackerspace in Adelaide.
+
+Stay tuned.
+
+## Tools
+
+* Ruby 2.0.0
+* Rails 4.0.0
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackerspace::Application.config.secret_key_base = '6d54f1f68826d983927b0fcef3e2ae731710b50fe626ca3c083a88e54a5da5315ccdca36257afba0bc386cc2276774ad57ad75296ef3a0bfe17d8ce0cae47785'
