dba2ce3933c9328cbc4e2ccb5e1b321b82ed8d8b
sighmon <simon.loffler@gmail.com>
2013-09-14 17:25:32 +0930

    Oops, bad bang.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -30,7 +30,7 @@ class RegistrationsController < Devise::RegistrationsController
 	private
 
 	def set_user
-		@user = User.find!(current_user)
+		@user = User.find(current_user)
 	end
 
 	def user_params
